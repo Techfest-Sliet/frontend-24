bbe8bc3fb1b54194e75c776390a60d03aa35f63d
Chitresh9876 <chitreshgupta9876@gmail.com>
2024-01-27 02:16:56 +0530

    added faq | particle bg | and sign-in btn action

--- src/components/Auth/SignIn/SignIn.js
+++ src/components/Auth/SignIn/SignIn.js
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import axios from "axios";
 import "./SignIn.css";
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
-import { TextField, Stack } from "@mui/material";
+import { TextField, Stack, Hidden } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
+import Particle from "./Particle";
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  backgroundColor: theme.palette.mode === "dark" ? "#ffffff" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(1),
   textAlign: "center",
@@ -23,7 +24,7 @@ const style = {
   background: "transparent",
   color: "white",
   //bgcolor: "background.paper",
-  //border: "2px solid #000",
+  border: "2px solid #fff",
   //boxShadow: 24,
   pt: 2,
   px: 4,
@@ -43,8 +44,8 @@ const SignIn = () => {
         password: password,
       })
       .then((res) => {
-        console.log(res?.data?.isError)
-        if(res?.data?.isError) alert(res?.data?.message)
+        console.log(res?.data?.isError);
+        if (res?.data?.isError) alert(res?.data?.message);
       })
       .catch((err) => {
         console.log(err);
@@ -52,75 +53,120 @@ const SignIn = () => {
   };
 
   return (
-    <div className="signInContainer">
-      <Modal
-        open={true}
-        aria-labelledby="child-modal-title"
-        aria-describedby="child-modal-description"
-        sx={{
-          border: "2px solid white",
-          letterSpacing: "2px",
-        }}
-      >
-        <Box sx={{ ...style, width: "auto", background: "rgba(0,0,0,0.3)" }}>
-          <h2 id="child-modal-title" style={{ whiteSpace: "nowrap" }}>
-            WELCOME BACK
-          </h2>
-          <p>EMAIL</p>
-          <TextField
-            id="outlined-basic"
-            label="EMAIL"
-            variant="outlined"
-            size="small"
-            InputProps={{
-              sx: {
-                color: "#ffffff",
-                border: "1px solid #ffffff",
-              },
-            }}
-            InputLabelProps={{
-              sx: {
-                color: "white",
-              },
-            }}
-            onChange={(e) => setUserEmail(e.target.value)}
-          />
-          <p>PASSWORD</p>
-          <TextField
-            id="outlined-basic"
-            label="PASSWORD"
-            variant="outlined"
-            size="small"
-            InputProps={{
-              sx: {
-                color: "#ffffff",
-                border: "1px solid #ffffff",
-              },
-            }}
-            InputLabelProps={{
-              sx: {
-                color: "white",
-              },
-            }}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Stack direction={{ xs: "column", sm: "row" }} spacing={4} mt={2}>
-            <p>Forgot Password?</p>
-            <Item
+    <>
+      <Particle />
+      <div className="signInContainer">
+        <Modal
+          open={true}
+          aria-labelledby="child-modal-title"
+          aria-describedby="child-modal-description"
+          style={{
+            border: "2px solid white",
+            letterSpacing: "2px",
+          }}
+        >
+          <Box sx={{ ...style, width: "auto", background: "rgba(0,0,0,0.3)" }}>
+            <h2 id="child-modal-title" style={{ whiteSpace: "nowrap" }}>
+              WELCOME BACK
+            </h2>
+            <p>EMAIL</p>
+            <TextField
+              id="outlined-basic"
+              // label="EMAIL"
+              variant="outlined"
+              size="small"
               sx={{
-                ":hover": {
-                  cursor: "pointer",
+                "& .MuiOutlinedInput-root": {
+                  "&.Mui-focused fieldset": {
+                    borderColor: "#ffffff",
+                    color: "#ffffff",
+                  },
+                  "&.Mui-focused hover": {
+                    borderColor: "#ffffff",
+                    color: "#ffffff",
+                  },
+                  "& fieldset": {
+                    borderColor: "#ffffff",
+                    color: "#ffffff",
+                  },
+                  "&:hover fieldset": {
+                    borderColor: "#ffffff",
+                    color: "#ffffff",
+                  },
                 },
               }}
-              onClick={handleSignIn}
-            >
-              Sign In
-            </Item>
-          </Stack>
-          <p mt={2}>Don't have a account? Sign Up</p>
-        </Box>
-      </Modal>
-    </div>
+              InputProps={{
+                sx: {
+                  color: "#ffffff",
+                  border: "1px solid #ffffff",
+                },
+              }}
+              InputLabelProps={{
+                sx: {
+                  color: "white",
+                },
+              }}
+              onChange={(e) => setUserEmail(e.target.value)}
+            />
+            <p>PASSWORD</p>
+            <TextField
+              id="outlined-basic"
+              // label="PASSWORD"
+              variant="outlined"
+              size="small"
+              sx={{
+                "& .MuiOutlinedInput-root": {
+                  "&.Mui-focused fieldset": {
+                    borderColor: "#ffffff",
+                    color: "#ffffff",
+                  },
+                  "&.Mui-focused hover": {
+                    borderColor: "#ffffff",
+                    color: "#ffffff",
+                  },
+                  "& fieldset": {
+                    borderColor: "#ffffff",
+                    color: "#ffffff",
+                  },
+                  "&:hover fieldset": {
+                    borderColor: "#ffffff",
+                    color: "#ffffff",
+                  },
+                },
+              }}
+              InputProps={{
+                sx: {
+                  color: "#ffffff",
+                  border: "1px solid #ffffff",
+                },
+              }}
+              InputLabelProps={{
+                sx: {
+                  color: "white",
+                },
+              }}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Stack direction={{ xs: "column", sm: "row" }} spacing={4} mt={2}>
+              <p style={{ marginTop: "0.5rem" }}>Forgot Password?</p>
+              <Item
+                sx={{
+                  ":hover": {
+                    cursor: "pointer",
+                  },
+                  fontWeight:"550",
+                  color:"black",
+                }}
+                onClick={handleSignIn}
+              >
+                Sign In
+              </Item>
+            </Stack>
+            <p mt={2}>Don't have a account? Sign Up</p>
+          </Box>
+        </Modal>
+      </div>
+    </>
   );
 };
 
--- src/components/FAQ/Faq.js
+++ src/components/FAQ/Faq.js
@@ -1,9 +1,117 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Faq.css";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram';
+
+const Card = ({ heading, details }) => {
+  return (
+    <div className="faqCardDetails">
+      ABOUT
+      <div className="cardHeading">{heading}</div>
+      {details}
+      <br />
+      <br />
+      <a href="/">Read More &gt;</a>
+    </div>
+  );
+};
 
 const Faq = () => {
+  const [index, setIndex] = useState(0);
+  const data = [
+    {
+      heading: "Login and Password1",
+      details:
+        "If you want to login click on the sign-in button and enter the details",
+      link: "/",
+    },
+    {
+      heading: "Login and Password2",
+      details:
+        "If you want to login click on the sign-in button and enter the details",
+      link: "/",
+    },
+    {
+      heading: "Login and Password3",
+      details:
+        "If you want to login click on the sign-in button and enter the details",
+      link: "/",
+    },
+    {
+      heading: "Login and Password4",
+      details:
+        "If you want to login click on the sign-in button and enter the details",
+      link: "/",
+    },
+    {
+      heading: "Login and Password5",
+      details:
+        "If you want to login click on the sign-in button and enter the details",
+      link: "/",
+    },
+    {
+      heading: "Login and Password6",
+      details:
+        "If you want to login click on the sign-in button and enter the details",
+      link: "/",
+    },
+    {
+      heading: "Login and Password7",
+      details:
+        "If you want to login click on the sign-in button and enter the details",
+      link: "/",
+    },
+    {
+      heading: "Login and Password8",
+      details:
+        "If you want to login click on the sign-in button and enter the details",
+      link: "/",
+    },
+  ];
+  const n = data.length;
+  const handleMinimise = () => {
+    if (index >0) setIndex(index-1);
+  };
+  const handleMaximise = () => {
+    if (index < n-3) setIndex(index+1);
+  };
   return (
-    <div>Faq Reset password</div>
-  )
-}
+    <div className="faqContainer">
+      <div className="faqHeading">
+        <span className="faq">F</span>requently <span className="faq">A</span>
+        sked <span className="faq">Q</span>uestions
+      </div>
+      <div className="faqSubHeading">
+        Explore our help community or learn more about techFest.
+      </div>
+      <div className="textScroller">
+        <div className="side-arrow" onClick={handleMinimise}>
+          &lt;
+        </div>
+        <Card
+          heading={data[index % n]?.heading}
+          details={data[index % n]?.details}
+        />
+        <Card
+          heading={data[index+1 % n]?.heading}
+          details={data[index+1 % n]?.details}
+        />
+        <Card
+          heading={data[index+2 % n]?.heading}
+          details={data[index+2 % n]?.details}
+        />
+        
+        <div className="side-arrow" onClick={handleMaximise}>
+          &gt;
+        </div>
+      </div>
+      <div className="faqFooter">
+        <div> &copy;2024 techFest. All Right Reserved </div>
+        <div className="faqFooterNav"> <a href="/">HOME</a><a href="/sign-in">SIGN-IN</a><a href="/sign-up">SIGN-UP</a> </div>
+        <div> <a href="https://www.linkedin.com/company/slietofficialtnp/"><LinkedInIcon/></a> <a href="https://www.instagram.com/slietofficialtnp?igsh=YzRic2F4c3k2aXVi"><InstagramIcon/></a></div>
+        </div>
+    </div>
+  );
+};
 
-export default Faq
+export default Faq;
