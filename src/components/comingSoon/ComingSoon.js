26e5114e91db29cf60a31bf6d380786c1983ba03
Chitresh9876 <chitreshgupta9876@gmail.com>
2024-01-28 16:14:10 +0530

    added coming soom page

---
+++ src/components/comingSoon/ComingSoon.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./ComingSoon.css";
+import Particle from "../Auth/SignIn/Particle";
+
+const ComingSoon = () => {
+  return (
+    <div className="coming-soon-body">
+        <Particle/>
+      <div className="content">
+        <h3>COMINGSOON</h3>
+        <h3>COMINGSOON</h3>
+      </div>
+    </div>
+  );
+};
+
+export default ComingSoon;
