69380ba8b83a9a0f7131547bb31f1c6357abcd1b
Chitresh9876 <chitreshgupta9876@gmail.com>
2024-01-28 14:08:56 +0530

    faq data added

--- src/components/FAQ/Faq.js
+++ src/components/FAQ/Faq.js
@@ -9,9 +9,6 @@ const Card = ({ heading, details }) => {
       ABOUT
       <div className="cardHeading">{heading}</div>
       {details}
-      <br />
-      <br />
-      <a href="/">Read More &gt;</a>
     </div>
   );
 };
@@ -20,54 +17,42 @@ const Faq = () => {
   const [index, setIndex] = useState(0);
   const data = [
     {
-      heading: "Login and Password1",
-      details:
-        "If you want to login click on the sign-in button and enter the details",
-      link: "/",
+        id: 1,
+        question: "How to register on the website?",
+        answer: (<p>You can enter your details and sign up at <a href='https://www.techfestsliet.org/' style={{ textDecoration: "none", color: "DodgerBlue", fontFamily: "productSans" }}> https://www.techfestsliet.org/</a></p>)
+
     },
     {
-      heading: "Login and Password2",
-      details:
-        "If you want to login click on the sign-in button and enter the details",
-      link: "/",
+        id: 2,
+        question: "When is techFEST'23?",
+        answer: "The techfest is scheduled to be held on 24th-25th March."
     },
     {
-      heading: "Login and Password3",
-      details:
-        "If you want to login click on the sign-in button and enter the details",
-      link: "/",
+        id: 3,
+        question: "Do I need to pay any fee for registration?",
+        answer: (<p>There is no registration fee for candidates enrolled in SLIET. For participants from other institutes the registration fee is: <li style={{fontFamily: "productSans"}}>Rs 299 for online events</li> <li style={{fontFamily: "productSans"}}>Rs 599 for offline events</li> </p>)
     },
     {
-      heading: "Login and Password4",
-      details:
-        "If you want to login click on the sign-in button and enter the details",
-      link: "/",
+        id: 4,
+        question: "How many teams can I be a part of for an event?",
+        answer: "For a given event, a participant can be a part of only one team."
     },
     {
-      heading: "Login and Password5",
-      details:
-        "If you want to login click on the sign-in button and enter the details",
-      link: "/",
+        id: 5,
+        question: "Can I participate in multiple events?",
+        answer: "Yes, you are encouraged to participate in as many events as you like. Also there are no departmental constraints i.e. students of a department can participate in of the other domains. "
     },
     {
-      heading: "Login and Password6",
-      details:
-        "If you want to login click on the sign-in button and enter the details",
-      link: "/",
+        id: 6,
+        question: "What is the registration process?",
+        answer: "All members have to register themselves on the website. The team leader will then for a team and add the remaining members. "
     },
     {
-      heading: "Login and Password7",
-      details:
-        "If you want to login click on the sign-in button and enter the details",
-      link: "/",
-    },
-    {
-      heading: "Login and Password8",
-      details:
-        "If you want to login click on the sign-in button and enter the details",
-      link: "/",
-    },
-  ];
+        id: 7,
+        question: "Can students of institutes other than SLIET participate in the events?",
+        answer: "Yes, students enrolled in diploma and degree programs in any technical institute can participate."
+    }
+]
   const n = data.length;
   const handleMinimise = () => {
     if (index >0) setIndex(index-1);
@@ -89,16 +74,16 @@ const Faq = () => {
           &lt;
         </div>
         <Card
-          heading={data[index % n]?.heading}
-          details={data[index % n]?.details}
+          heading={data[index % n]?.question}
+          details={data[index % n]?.answer}
         />
         <Card
-          heading={data[index+1 % n]?.heading}
-          details={data[index+1 % n]?.details}
+          heading={data[index+1 % n]?.question}
+          details={data[index+1 % n]?.answer}
         />
         <Card
-          heading={data[index+2 % n]?.heading}
-          details={data[index+2 % n]?.details}
+          heading={data[index+2 % n]?.question}
+          details={data[index+2 % n]?.answer}
         />
         
         <div className="side-arrow" onClick={handleMaximise}>
@@ -106,7 +91,7 @@ const Faq = () => {
         </div>
       </div>
       <div className="faqFooter">
-        <div> &copy;2024 techFest. All Right Reserved </div>
+        <div> &copy;2024 techFest'24. All Right Reserved </div>
         <div className="faqFooterNav"> <a href="/">HOME</a><a href="/sign-in">SIGN-IN</a><a href="/sign-up">SIGN-UP</a> </div>
         <div> <a href="https://www.linkedin.com/company/slietofficialtnp/"><LinkedInIcon/></a> <a href="https://www.instagram.com/slietofficialtnp?igsh=YzRic2F4c3k2aXVi"><InstagramIcon/></a></div>
         </div>
