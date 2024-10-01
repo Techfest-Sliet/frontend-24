986060f519a1a2818693740dd21667f4b94defcd
Sandeep Kumar <sandeepqmar05@gmail.com>
2024-10-01 15:16:06 +0530

    Changed the API url

--- src/API/api.js
+++ src/API/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//export const baseUrl = `/api`;
-export const baseUrl = 'http://localhost:3000';
+export const baseUrl = `/api`;
+// export const baseUrl = 'http://localhost:3000';
 
 export const localUrlIns = axios.create({
   baseURL: baseUrl,
