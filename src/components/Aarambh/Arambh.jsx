cb77c32b992e314b05606b5c5ce0bb745ed915e3
anwaraftab007 <anwaraftab007@gmail.com>
2024-10-05 11:51:30 +0530

    Fix aarambh componenet

--- src/components/Aarambh/Arambh.jsx
+++ src/components/Aarambh/Arambh.jsx
@@ -4,27 +4,6 @@ import StarCanvas from "../../screens/landingPage/StarbackGround";
 import { Box } from "@mui/material";
 import { Link } from "react-router-dom";
 import {baseUrl} from "../../API/api"
-const Workshop = [{
-  id: 1,
-  name: "Olympiad – Quiz 1",
-  description: "A quiz will be conducted for students of different schools in nearby areas to encourage creativity and awareness about trending tech and software. All participating students will receive e-certificates, and the top 3 students will get winning certificates in the techfest.",
-  start_time: "2024-10-03T10:00:00",
-  end_time: "2024-10-05T10:00:00"
-},
-{
-  id: 2,
-  name: "Olympiad – Quiz 2",
-  description: "A quiz will be conducted for students of different schools in nearby areas to encourage creativity and awareness about trending tech and software. All participating students will receive e-certificates, and the top 3 students will get winning certificates in the techfest.",
-  start_time: "2024-09-28T10:00:00",
-  end_time: "2024-09-30T10:00:00"
-},
-{
-  id: 3,
-  name: "Olympiad – Quiz 3",
-  description: "A quiz will be conducted for students of different schools in nearby areas to encourage creativity and awareness about trending tech and software. All participating students will receive e-certificates, and the top 3 students will get winning certificates in the techfest.",
-  start_time: "2024-10-08T10:00:00",
-  end_time: "2024-10-13T10:00:00"
-}]
 
 const aarambhEvents = await fetch(`${baseUrl}/workshop`)
 .then(response => response.json());
