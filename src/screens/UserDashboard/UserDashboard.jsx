3b718fa971d1e34ca6dab7c28ad61d91beed4561
Sandeep Kumar <sandeepqmar05@gmail.com>
2024-10-14 23:34:34 +0530

    Using Loader instead of Error

--- src/screens/UserDashboard/UserDashboard.jsx
+++ src/screens/UserDashboard/UserDashboard.jsx
@@ -656,7 +656,7 @@ const UserDashboard = () => {
                 </div>
             ) : (
                 <>
-                    <Error />
+                    <Loader />
                 </>
             )
             }
